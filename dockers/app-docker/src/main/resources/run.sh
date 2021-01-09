7bd8974a0ad1f0324cf5036d0f22091f7c403092
arshads96 <arshads96@gmail.com>
2021-01-10 01:51:17 +0530

    Docker code for customer service module

--- dockers/app-docker/src/main/resources/run.sh
+++ dockers/app-docker/src/main/resources/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo "I'm in docker image"
+
+cd $HOME
+
+java -agentlib:jdwp=transport=dt_socket,address=8090,server=y,suspend=n -jar ./app-0.0.1-SNAPSHOT.jar
+
+echo "Customer Service module successfully deployed!!"
